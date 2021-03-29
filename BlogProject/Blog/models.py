e3440527be14c8f5311c1a9957fc64d588157bc4
hossainchisty <hossain.chisty11@gmail.com>
2021-03-29 17:52:26 +0600

    Update TextField to RichTextUploadingField

--- BlogProject/Blog/models.py
+++ BlogProject/Blog/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from ckeditor_uploader.fields import RichTextUploadingField
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    body = models.TextField()
+    body = models.RichTextUploadingField()
     published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(  
+        User,
+         on_delete=models.CASCADE)
 
     class Meta:
         verbose_name_plural = 'Post'
