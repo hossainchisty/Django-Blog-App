26c85916279513129a83fb3bdc5e424c367256ca
hossainchisty <hossain.chisty11@gmail.com>
2021-03-31 23:37:41 +0600

    last update was slugfield

--- BlogProject/Blog/models.py
+++ BlogProject/Blog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from autoslug import AutoSlugField
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
+    slug = AutoSlugField(populate_from='title',null=True)
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(  
         User,
--- BlogProject/Blog/urls.py
+++ BlogProject/Blog/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('',HomeView.as_view(), name='home'),
-    path('post_detail/<int:pk>' ,PostView.as_view(), name="post_detail"),
+    path('post_detail/<slug:post>' ,views.post_detail, name="post_detail"),
     path('post_create/',CreatePostView.as_view(), name="create_post"),
     path('post_update/<pk>', POSTUpdateView.as_view(), name="post_edit"),
     path('post_delete/<pk>', POSTDeleteView.as_view(), name="post_delete"),
